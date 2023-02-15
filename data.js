b7e86cf4ed927e2ca267e12994305b6e810463b2
Jozhua Simeon <jmesimeon.cvt@eac.edu.ph>
2023-02-15 15:24:01 +0800

    marzipano

---
+++ data.js
@@ -0,0 +1,491 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-b1f2hall1west-edited",
+      "name": "B1F2Hall1West EDITED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2885.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6048982316487317,
+          "pitch": 0.6285826509351935,
+          "rotation": 0,
+          "target": "1-b1f2hall2west-edited"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-b1f2hall2west-edited",
+      "name": "B1F2Hall2West EDITED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2896.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1893420401961965,
+          "pitch": 0.4933909943859156,
+          "rotation": 0,
+          "target": "2-b1f2hall-west-edited"
+        },
+        {
+          "yaw": -0.9601975621134411,
+          "pitch": 0.4655859674896181,
+          "rotation": 0,
+          "target": "0-b1f2hall1west-edited"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-b1f2hall-west-edited",
+      "name": "B1F2Hall West EDITED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2891,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0413506536869654,
+          "pitch": 0.5244160072689628,
+          "rotation": 0,
+          "target": "1-b1f2hall2west-edited"
+        },
+        {
+          "yaw": 0.12775892834295277,
+          "pitch": 0.42544506077389777,
+          "rotation": 0,
+          "target": "3-bulwaganwest"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bulwaganwest",
+      "name": "BulwaganWest",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2887.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.68548902915472,
+          "pitch": 0.40427177483133114,
+          "rotation": 0,
+          "target": "4-bulwagan"
+        },
+        {
+          "yaw": 2.4161956747269127,
+          "pitch": 0.4579539035540918,
+          "rotation": 0,
+          "target": "5-bulwaganeast"
+        },
+        {
+          "yaw": -0.7107073033083999,
+          "pitch": 0.6217339710346366,
+          "rotation": 0,
+          "target": "2-b1f2hall-west-edited"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bulwagan",
+      "name": "Bulwagan",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2897,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5523325017851128,
+          "pitch": 0.5073166045943207,
+          "rotation": 0,
+          "target": "5-bulwaganeast"
+        },
+        {
+          "yaw": 0.48988965685494357,
+          "pitch": 0.5159510530389166,
+          "rotation": 0,
+          "target": "3-bulwaganwest"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bulwaganeast",
+      "name": "BulwaganEast",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2887,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.896026339898576,
+          "pitch": 0.7806761788790588,
+          "rotation": 0,
+          "target": "6-b1f2hall4-east-edited"
+        },
+        {
+          "yaw": -0.2740680449408366,
+          "pitch": 0.34887745825475847,
+          "rotation": 0,
+          "target": "4-bulwagan"
+        },
+        {
+          "yaw": -1.3502372486488898,
+          "pitch": 0.3473341338149307,
+          "rotation": 0,
+          "target": "3-bulwaganwest"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-b1f2hall4-east-edited",
+      "name": "B1F2Hall4 East EDITED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2894.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.581783380836905,
+          "pitch": 0.5050369571955535,
+          "rotation": 0,
+          "target": "7-b1f2hall3-east-edited"
+        },
+        {
+          "yaw": 0.5072720708537481,
+          "pitch": 0.5775516464753316,
+          "rotation": 0,
+          "target": "5-bulwaganeast"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-b1f2hall3-east-edited",
+      "name": "B1F2Hall3 East EDITED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2890,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6610509283528465,
+          "pitch": 0.40122321231079283,
+          "rotation": 0,
+          "target": "6-b1f2hall4-east-edited"
+        },
+        {
+          "yaw": 2.47487060179842,
+          "pitch": 0.7130717417661376,
+          "rotation": 0,
+          "target": "8-b1f2hall2-edited"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-b1f2hall2-edited",
+      "name": "B1F2Hall2 EDITED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2903.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8949202727383039,
+          "pitch": 0.5461889313019839,
+          "rotation": 0,
+          "target": "9-b1f2hall1east"
+        },
+        {
+          "yaw": 2.242081131226234,
+          "pitch": 0.5692211425747438,
+          "rotation": 0,
+          "target": "7-b1f2hall3-east-edited"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-b1f2hall1east",
+      "name": "B1F2Hall1East",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2907,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3067863426405282,
+          "pitch": 0.4965555808962989,
+          "rotation": 0,
+          "target": "8-b1f2hall2-edited"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Dayum",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
--- sidemenubar.js
+++
@@ -1,8 +0,0 @@
-function openSM(){
-    document.getElementById("mySidemenu").style.width ="10%";
-    document.getElementById("pg-content").style.marginLeft = "10%";
-}
-function closeSM(){
-    document.getElementById("mySidemenu").style.width = "0";
-    document.getElementById("pg-content").style.marginLeft ="0";
-}
